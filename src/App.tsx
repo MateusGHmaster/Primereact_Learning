0508110947063d148ec91877ef52426edc58426f
mateusghmaster <mateusoliveira469@gmail.com>
2022-10-19 08:07:42 -0400

    feat: material ui theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import React, { useRef, useState } from 'react';
 import logo from './logo.png';
 import './App.css';
 
-import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
-import "primereact/resources/primereact.min.css";                  //core css
+import "primereact/resources/themes/md-dark-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";               //core css
 import "primeicons/primeicons.css";
+import "/node_modules/primeflex/primeflex.css";
 
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
@@ -14,31 +15,30 @@ function App() {
 
     const [input, setInput] = useState('');
 
-    const toastRef: any = useRef(null);
+    const toastRef: any = useRef();
     const onButtonClick = () => {
         if (input.length > 0) {
             toastRef.current.show({severity:'success', summary: 'Successo!', detail:'Há um texto mínimo a ser submetido.', life: 3000});
         } else {
-            toastRef.current.show({severity:'error', summary: 'Falha!', detail:'Não foi atingido a quantidade mínima de caracteres, para realizar uma submnissão.', life: 3000});
+            toastRef.current.show({severity:'error', summary: 'Falha!', detail:'Não foi atingido a quantidade mínima de caracteres, para realizar uma submissão.', life: 3000});
         }
     }
 
     return (
 
-    <div className="App">
+        <div className="App">
 
-        <Toast ref={toastRef} />
+            <Toast ref={toastRef} />
 
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <InputText placeholder="Insert Prime Text" value={input} onChange={(e) => setInput(e.target.value)} />
-            <p>{input}</p>
-            <Button icon="pi pi-check" label='Submit' onClick={onButtonClick}/>
-            <p>
-            PrimeReact Teste Code
-            </p>
-        </header>
-    </div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <span className="p-float-label mb-5">
+                    <InputText value={input} onChange={(e) => setInput(e.target.value)} />
+                    <label htmlFor="input_txt" className="p-component">Primereact Text</label>
+                </span>
+                <Button icon="pi pi-check" label='Submit' onClick={onButtonClick}/>
+            </header>
+        </div>
 
     );
 }
